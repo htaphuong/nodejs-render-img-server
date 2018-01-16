a97371af1a99566c5b424f0d99a6f2c216c70485
htaphuong <htaphuong@apcs.vn>
2018-01-16 22:49:22 +0700

    add "uploading image/file with mult"

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const _ = require('lodash');
+const path = require('path')
+const multer = require('multer')
 
 const app = express();
 
@@ -15,3 +17,44 @@ app.get('/', (req, res) =>
 
 const port = process.env.PORT || 9999;
 app.listen(port);
+
+// Upload image
+const allowTypes = ['image/png', 'image/jpeg', 'image/gif'];
+const uploadConfig = {
+    fields: 17,
+    files: 17,
+    fileSize: 100 * 1048576,
+    parts: 17
+};
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        var s = path.resolve(__dirname, 'public/resource'); 
+        //console.log(Boolean(allowTypes.indexOf('ava01.png') > -1))
+        //console.log('testttt')
+        cb(null, `${path.resolve(__dirname, 'public/resource')}`);
+    },
+    
+    filename(req, { originalname, mimetype }, cb) {
+        //console.log({mimetype})
+        const nameSegments = originalname.split('.');
+        const name = nameSegments[0] || `${Date.now()}`;
+
+        const mineTypeSegments = mimetype.split('/');
+        const ext = mineTypeSegments[1] || 'jpeg';
+        cb(null, `${Date.now()}-${name}.${ext}`);
+    }
+});
+
+const fileFilter = (req, { mimetype }, cb) => {
+    //console.log({mimetype})
+    cb(null, Boolean(allowTypes.indexOf(mimetype) > -1));
+}
+
+const uploader = multer({ storage, fileFilter, limits: uploadConfig });
+
+app.post('/upload', uploader.array('iImages'), (req, res) => {
+    //console.log(req)
+    res.json({ images: req.files })
+}
+);
